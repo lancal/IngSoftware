91a9fea6988b2a8820ebba211ea3f64874753c6e
Alan <ivda.calderon@gmail.com>
2018-12-27 13:10:32 -0300

    Creacion Role y User Seeder

---
+++ database/seeds/Role_Seeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class Role_Seeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
