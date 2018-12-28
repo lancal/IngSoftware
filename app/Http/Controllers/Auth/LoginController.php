b428e456feb678d093b5bc6f1a9b58614dcc6221
Alan <ivda.calderon@gmail.com>
2018-12-27 23:08:57 -0300

    Creacion Role y User Seeder

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -28,7 +28,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/';
+    protected $redirectAfterLogout = '/';
 
     /**
      * Create a new controller instance.
@@ -47,7 +47,7 @@ class LoginController extends Controller
         Auth::logout();
         Session::flush();
         //return redirect(property_exists($this, 'redirectAfterLogout') ? $this->redirectAfterLogout : '/');
-        return redirect(property_exists($this, 'redirectAfterLogout') ?$this->redirectAfterLogout : '/');
+        return redirect(property_exists($this, 'redirectAfterLogout') ? $this->redirectAfterLogout : '/');
         //return redirect('login');
     }
 }
