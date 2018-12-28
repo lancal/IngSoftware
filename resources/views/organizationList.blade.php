757c6b4c8b8d86cc40c51335514c4a35cd09ccbc
nilsson3107 <38134347+nilsson3107@users.noreply.github.com>
2018-12-28 10:07:36 -0300

    arreglo listado organizaciones
    se utiliza container y card bootstrap

--- resources/views/organizationList.blade.php
+++ resources/views/organizationList.blade.php
@@ -5,28 +5,35 @@
 
 @endsection
 @section('contenido')</br>
+    <div class="container">
 
-    <table>
-        <thead>
-        <tr>
+        <div class="card">
+            <h4 class="card-header"> Listado de Organizaciones</h4>
+            <div class="card-body">
+                <table>
+                    <thead>
+                    <tr>
 
-            <th> Nombre</th>
-            <th> Responsable  </th>
-            <th> Rut </th>
-            <th> Tipo Empresa</th>
-        </tr>
-        </thead>
-        <tbody>
-        @foreach($organizaciones as $organizations)
-            <tr>
+                        <th> Nombre</th>
+                        <th> Responsable</th>
+                        <th> Rut</th>
+                        <th> Tipo Empresa</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @foreach($organizaciones as $organizations)
+                        <tr>
 
-                <td> {{$organizations->nombre}} </td>
-                <td> {{$organizations->responsable}} </td>
-                <td> {{$organizations->rut}} </td>
-                <td> {{$organizations->tipoEmpresa}} </td>
-            </tr>
-        @endforeach
-        </tbody>
-    </table>
+                            <td> {{$organizations->nombre}}</td>
+                            <td> {{$organizations->responsable}}</td>
+                            <td> {{$organizations->rut}}</td>
+                            <td> {{$organizations->tipoEmpresa}}</td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 
 @endsection
